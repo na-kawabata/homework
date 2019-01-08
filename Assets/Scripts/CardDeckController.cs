28a5196f6f2dcd8ce151f70e1d81c767fe5cd4b2
na-kawabata <46367107+na-kawabata@users.noreply.github.com>
2019-01-08 17:54:18 +0900

    ページ追加

---
+++ Assets/Scripts/CardDeckController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDeckController : MonoBehaviour {
+
+    [SerializeField] GameObject deckCardPre;//deckCardPrefab
+
+
+    //カード生成
+    public void DeckCardList()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject obj = Instantiate(cardPre, cardPanel.transform);
+        }
+
+    }
+
+    //カードタップ時情報ポップアップ表示
+    public void TapCard()
+    {
+        if (popup)
+        {
+            Destroy(popup);
+        }
+        //中央にカード詳細popupを表示
+        popup = Instantiate(popupPre, this.
+
+
+}
